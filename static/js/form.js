db39650a65a96b7f667a16fedb21c186a2660bc8
rickirini <30579210+rickirini@users.noreply.github.com>
2019-05-08 15:05:23 +0200

    Added Jquery for dynamic url

--- static/js/form.js
+++ static/js/form.js
@@ -1,25 +1,48 @@
-$("#click-button").on('switch', firstClick)
+// $("#click-button").on('switch', firstClick)
 
-function firstClick() {
-    alert("First Clicked");
-    $("#click-button").off('switch').on('click', secondClick)
-}
+// function firstClick() {
+// 	alert("First Clicked");
+// 	$("#click-button").off('switch').on('click', secondClick)
+// }
 
-function secondClick() {
-    alert("Second Clicked");
-    $("#click-button").off('switch').on('click', firstClick)
-}
+// function secondClick() {
+// 	alert("Second Clicked");
+// 	$("#click-button").off('switch').on('click', firstClick)
+// }
+// var state = "nl-gro"
+// function state_1() {
+// 	if (state == "nl-gro") {
+// 		state = "gro-nl";
+// 	} else {
+// 		state = "nl-gro";
+// 	}
+// 	document.getElementById("demo").innerHTML = state;
+// 	// var myElement = $("demo")= state ; the Jquery variant
+
+// }
+
+var state = "nl-gro"
+
+$(document).ready(function(){
+  $(".toggle").click(function(){
+    if (state == "nl-gro") {
+        state = "gro-nl";
+      } else {
+        state = "nl-gro";
+      }
+    // $(".status").html(state);
+    console.log(state);
+  });
+});
 
 $(document).ready(function() {
-
 	$('form').on('submit', function(event) {
-
 		$.ajax({
 			data : {
 				name : $('#nameInput').val()			
 			},
 			type : 'POST',
-			url : '/predict'
+			url : '/predict_'+state
 		})
 		.done(function(data) {
 
@@ -39,3 +62,22 @@ $(document).ready(function() {
 	});
 
 });
+
+// $(document).ready(function() {
+// 	$('.toggle').click(function() {
+// 		var current_status = $('.status').text();
+// 		$.ajax({
+// 			url: "/get_toggled_status",
+// 			type: "get",
+// 			data: {status: current_status},
+// 			success: function(response) {
+// 				$(".status").html(response);
+// 				console.log(state);
+
+// 			},
+// 			error: function(xhr) {
+//       //Do Something to handle error
+//   }
+// });
+// 	});
+// });
---
+++ static/js/try.js
@@ -0,0 +1,11 @@
+
+function state() {
+	var state = "nl-gro";
+	if (state == "nl-gro") {
+		state = "gro-nl";
+  //  block of code to be executed if the condition is true
+} else { 
+	state == "nl-gro";
+  //  block of code to be executed if the condition is false
+}
+}
