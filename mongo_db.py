accdfb65c2c9f5e99d98d3ad1d86909f9ab43d59
rickkosse <r.kosse1993@gmail.com>
2020-05-11 20:38:19 +0200

    added db fields

--- mongo_db.py
+++ mongo_db.py
@@ -1,16 +1,20 @@
 from creds_mongo import uri
 from mongoengine import *
-import datetime
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 def connect_to_mongo():
     connect('annotater', host=uri)
 
     class Gronings_Annotation(Document):
-        _id = IntField()
+        _id = ObjectIdField()
         orginal_gronings = StringField()
-        annotated_gronings = ListField(null=True)
+        annotated_gronings = ListField(StringField(null=True))
         best_pick = ListField(null=True)
+        is_annotated = BooleanField(default=False)
 
     return Gronings_Annotation
 
@@ -25,9 +29,18 @@ def insert_to_db(sentence):
 
 
 def get_annotation():
-    instance = Gronings_Annotation.objects.first()
-    return [instance.orginal_gronings]
+    # instance = Gronings_Annotation.objects.first()
+    return Gronings_Annotation.objects.first()
 
 
 def get_all_annotation():
-    return [instance.orginal_gronings for instance in Gronings_Annotation.objects()]
+    return Gronings_Annotation.objects(annotated_gronings__3__exists=False)
+
+
+def get_all_validations():
+    return Gronings_Annotation.objects(Q(annotated_gronings__ne="") and Q(best_pick=None))
+
+
+def store_anno_in_mongo(sentence, ids):
+    Gronings_Annotation.objects(_id=ids).update(annotated_gronings=sentence)
+    logging.info("Updated DB")
